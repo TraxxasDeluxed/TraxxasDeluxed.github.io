c4c6c4123fc177f0152d6db183f71e75d05668c4
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-04-17 16:01:26 +0000

    errors fixed

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-class CurrentDate extends HTMLElement {
-    // The browser calls this method when the element is
-    // added to the DOM.
-    connectedCallback() {
-        // Create a Date object representing the current date.
-        const now = new Date();
-        
-        // Format the date to a human-friendly string, and set the
-        // formatted date as the text content of this element.
-        this.textContent = now.toLocaleDateString();
-    }
-}
-
-// Register the CurrentDate component using the tag name <current-date>.
-customElements.define('current-date', CurrentDate);
 class NavBar extends HTMLElement {
     // The browser calls this method when the element is
     // added to the DOM.
@@ -20,7 +5,7 @@ class NavBar extends HTMLElement {
         this.id = "navbar";
         this.innerHTML = `
         <ul>
-            <li><a href="#">Logo</a></li>
+            <li><a href="index.html">Logo</a></li>
             <li><a href="/about.html">About</a></li>
             <li><a href="/animations.html">Work</a></li>
             <li><a href="#">Contact</a></li>
