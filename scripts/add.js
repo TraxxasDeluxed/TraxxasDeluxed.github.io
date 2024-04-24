2a7cbd40d7f498ed283025ed8704fe2062303937
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-04-24 21:21:48 +0000

    Task of js math completed

---
+++ scripts/add.js
@@ -0,0 +1,29 @@
+// Task1
+for(let i=0;i<11;i++){
+    console.log(i*i)
+}
+
+//Task 2 
+for(let i=5;i>0;i--){
+    console.log(i);
+}
+console.log("Blastoff!")
+
+//Task 3
+for(let i=0;i<51;i+2){
+    console.log(i)
+}
+
+// Task 4 
+let sum=0
+for(let i=0;i<101;i++){
+    console.log(sum+i)
+}
+
+//Task 5
+
+let counter = 1
+for(i=1;i<11;i++){
+    counter*=1
+    console.log(counter)
+}
