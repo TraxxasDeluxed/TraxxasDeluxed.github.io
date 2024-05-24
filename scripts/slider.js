76e307fe553b0de40af5a43579d57d339c728186
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-05-24 13:02:13 -0700

    testing in progress

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,13 +1,12 @@
 let next = document.querySelector('.next')
 let prev = document.querySelector('.prev')
 
-next.addEventListener('click',function(){
+next.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide').appendChild(items[0])
 })
 
-prev.addEventListener('click',function(){
+prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[items.length-1]) // here the length of the items = 6
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
 })
-
