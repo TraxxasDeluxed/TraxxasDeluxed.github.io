88c9f155278644b4bcff63e8c1d7ee120b35b9fd
Ayansh Singh <ayanshashish@gmail.com>
2024-06-04 23:00:05 -0700

    updated the nav bar, contactUs and about ui, final product -- add final comments to code

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,9 @@ class NavBar extends HTMLElement {
     // added to the DOM.
     connectedCallback() {
         this.id = "navbar";
-   /*     const list = document.createElement("ul");
-        const links = {
-            Logo: "/index.html",
-            About: "/about.html"
-        };
-        for (const text in links){
-            const listItem = document.createElement()
-        }*/
         this.innerHTML = `
         <ul>
-            <li><a href="/index2.html">Ayansh Singh</a></li>
+            <li><a href="/index.html">Ayansh Singh</a></li>
             <li><a href="/about.html">About</a></li>
             <li><a href="/animations.html">Work</a></li>
             <li><a href="/contactUs.html">Contact</a></li>
